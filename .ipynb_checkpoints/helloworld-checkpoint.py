9b7f9008191f1bbab448073ca0487b67ac27ac2e
lekhanapotla <lekhana.pmk@gmail.com>
2020-08-08 12:38:52 +0530

    helloworld file added

---
+++ .ipynb_checkpoints/helloworld-checkpoint.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.5.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+print('Hi ')
+print('hello')
+
+print('2+3')
+
+
---
+++ .ipynb_checkpoints/sample-checkpoint.py
@@ -0,0 +1,7 @@
+print('Hello world')
+print('5+7')
+print('written in pupyternotebook')
+
+print('hello')
+
+
