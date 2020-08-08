0b07af396bf6dc46439221a03e4061348e3f220d
lekhanapotla <lekhana.pmk@gmail.com>
2020-08-08 12:35:02 +0530

    helloworld file added

---
+++ helloworld.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.5.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+print('Hi ')
+print('hello')
+
+print(2+3)
+
+for i in range(0,10):
+    print('hello')
+
+print('sample test')
+
+
